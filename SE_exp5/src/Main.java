8865c1937b4e7411e33b7f2f6a828e96f7c59b90
eeegoose <461420770@.com>
2025-11-27 12:30:48 +0800

    add tag v1.0

--- SE_exp5/src/Main.java
+++ SE_exp5/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!I am studying git.");
         System.out.println("Git Branch:来自 userA 的修改");
+        System.out.println("add tag v1.0");
     }
 }
